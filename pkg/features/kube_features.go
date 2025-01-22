0f5e6356acb930038287f8e20a5023d9c48e2cb6
Markus Lehtonen <markus.lehtonen@intel.com>
2025-01-22 15:10:47 +0200

    Add KubeletCRIResourceDiscovery feature gate

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -463,6 +463,12 @@ const (
 	// access for different tenants.
 	KubeletEnsureSecretPulledImages featuregate.Feature = "KubeletEnsureSecretPulledImages"
 
+	// owner: @marquiz
+	// kep: TBA
+	//
+	// Enable discovery of node resources from the CRI.
+	KubeletCRIResourceDiscovery featuregate.Feature = "KubeletCRIResourceDiscovery"
+
 	// owner: @vinayakankugoyal
 	// kep: http://kep.k8s.io/2862
 	//
@@ -1359,6 +1365,10 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	},
 
 	KubeletEnsureSecretPulledImages: {
+		{Version: version.MustParse("1.33"), Default: false, PreRelease: featuregate.Alpha},
+	},
+
+	KubeletCRIResourceDiscovery: {
 		{Version: version.MustParse("1.33"), Default: false, PreRelease: featuregate.Alpha},
 	},
 
